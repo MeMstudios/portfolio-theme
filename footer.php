edb898c4644803e8c5414fea964a61ca4d6ad57e
MeMstudios <michaelmurphystudios@gmail.com>
2023-10-25 09:48:34 -0400

    class right email

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-    <p>&copy; 2023 Michael E. Murphy michaelmurphystudios@gmail.com <!--a class="right" href='/contact'>Contact Me</a--></p>
+    <p>&copy; 2023 Michael E. Murphy <p class="right">michaelmurphystudios@gmail.com<p> <!--a class="right" href='/contact'>Contact Me</a--></p>
 </div>
 <script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.js'></script>
 <script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/js/custom.js'></script>
