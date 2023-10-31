a5f6d0842b4067d731cac46198a458edfacf971c
MeMstudios <michaelmurphystudios@gmail.com>
2023-10-31 15:37:07 -0400

    add email id to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-    <p>&copy; 2023 Michael E. Murphy <a class="right">michaelmurphystudios@gmail.com<a> <!--a class="right" href='/contact'>Contact Me</a--></p>
+    <p>&copy; 2023 Michael E. Murphy <a id="email" class="right">michaelmurphystudios@gmail.com<a> <!--a class="right" href='/contact'>Contact Me</a--></p>
 </div>
 <script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.js'></script>
 <script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/js/custom.js'></script>
